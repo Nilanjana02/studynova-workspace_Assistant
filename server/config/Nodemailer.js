68d7e3eb9c19deab318f74e12030c87a7fb1bccb
Nilanjana Maity <160305229+Nilanjana02@users.noreply.github.com>
2026-03-11 17:52:27 +0530

    Update Nodemailer.js secure,tls

--- server/config/Nodemailer.js
+++ server/config/Nodemailer.js
@@ -4,10 +4,14 @@ const transpoter = nodemailer.createTransport({
  
     host:'smtp-relay.brevo.com',
     port:587,
+    secure: false, 
     auth:{
         user:process.env.SMTP_USER,
         pass:process.env.SMTP_PASSWORD 
-    }
+    },
+   tls: {
+    rejectUnauthorized: false
+  }
 
 });
 transpoter.verify((error, success) => {
